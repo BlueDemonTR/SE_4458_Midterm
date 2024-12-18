6aa6f6d41b137481b8b00d5c47fabd6040e1c980
BlueDemonTR <83763146+BlueDemonTR@users.noreply.github.com>
2024-12-18 13:52:41 +0300

    add docker

--- swagger/service-a.js
+++ swagger/service-a.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -7,12 +6,9 @@ var cors = require('cors')
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
-var indexRouter = require('./routes/index');
-import config from './config'
-
-config.startDatabase()
-
 var app = express();
+
+var port = 3001
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,23 +25,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', indexRouter);
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.listen(port, () => {
+	console.log(`Service A listening on port ${port}`);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
